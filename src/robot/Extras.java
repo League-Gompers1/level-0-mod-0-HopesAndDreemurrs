5d7c215779d591b6bc9881d569eeda4e51ba91bb
HopesAndDreemurrs <yonaimygaming@gmail.com>
2017-04-17 14:58:18 -0700

    Add files via upload
    hi

---
+++ src/robot/Extras.java
@@ -0,0 +1,37 @@
+package robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Extras {
+	   public static void main(String[] args) throws Exception {
+		   Robot MTT = new Robot("batman");
+		   MTT.setSpeed(10);
+		   MTT.penDown();
+		   MTT.move(100);
+		   MTT.turn(90);
+		   MTT.move(100);
+		   MTT.turn(90);
+		   MTT.move(100);
+		   MTT.turn(90);
+		   MTT.move(100);
+		   MTT.turn(90);
+		   MTT.move(100);
+		   MTT.turn(-90);
+		   MTT.move(20);
+		   MTT.turn(120);
+		   MTT.move(150);
+		   MTT.turn(120);
+		   MTT.move(150);
+		   MTT.turn(120);
+		   MTT.move(20);
+		   MTT.turn(-90);
+		   MTT.move(100);
+		   MTT.turn(90);
+		   MTT.move(25);
+		   MTT.turn(90);
+		   MTT.move(25);
+		   MTT.turn(90);
+		   MTT.move(25);
+		   MTT.turn(90);
+}
+}
